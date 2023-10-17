4e459a1b6a97af003100cd0a383fbe411da037a8
Abugahh <cynthia.abugaa@gmail.com>
2023-10-17 21:13:19 +0300

    New ttrcpt

--- capture_pkt.py
+++ capture_pkt.py
@@ -1,20 +1,49 @@
-# import pyshark
-# import sys
+# # import pyshark
+# # import sys
 
-# # get the network interface from the command line arguments
-# interface = sys.argv[1]
+# # # get the network interface from the command line arguments
+# # interface = sys.argv[1]
+
+# # # create a capture object
+# # capture = pyshark.LiveCapture(interface=interface)
+
+# # # iterate over captured packets
+# # for packet in capture.sniff_continuously():
+# #     # print the captured packet
+# #     print(packet)
+
+
+# import pyshark
+# import threading
+
+# # specify the network interface to capture packets from
+# interface = 'wlan0'
 
 # # create a capture object
 # capture = pyshark.LiveCapture(interface=interface)
 
-# # iterate over captured packets
-# for packet in capture.sniff_continuously():
-#     # print the captured packet
-#     print(packet)
+# def capture_packets():
+#     # iterate over captured packets
+#     for packet in capture.sniff_continuously():
+#         # print the captured packet
+#         print(packet)
+
+# # create and start a thread to run the packet capture and processing code
+# t = threading.Thread(target=capture_packets)
+# t.start()
+
+
+
+# ### save as a script and call that script in my project.ipnb
+# ###  see in bing chat straight
+
+
 
 
 import pyshark
 import threading
+import datetime
+import math
 
 # specify the network interface to capture packets from
 interface = 'wlan0'
@@ -22,17 +51,112 @@ interface = 'wlan0'
 # create a capture object
 capture = pyshark.LiveCapture(interface=interface)
 
+# create a dictionary to store the features for each packet
+features = {}
+
+# create some helper functions to calculate some features
+
+def get_rate(packet):
+    # get the rate of packets per second
+    # assuming that the packets are ordered by time
+    # rate = 1 / interarrival time
+    global prev_time # keep track of the previous packet time
+    if prev_time is None:
+        # first packet, rate is zero
+        rate = 0
+    else:
+        # calculate the interarrival time in seconds
+        delta = packet.sniff_time - prev_time
+        interarrival = delta.total_seconds()
+        if interarrival == 0:
+            # avoid division by zero, rate is infinity
+            rate = math.inf
+        else:
+            # calculate the rate
+            rate = 1 / interarrival
+    # update the previous packet time
+    prev_time = packet.sniff_time
+    return rate
+
+def get_smean(packet):
+    # get the mean size of packets sent by source IP address
+    # assuming that the packets are grouped by source IP address
+    global src_size # keep track of the total size of packets sent by source IP address
+    global src_count # keep track of the number of packets sent by source IP address
+    if src_size is None or src_count is None:
+        # first packet, initialize the variables
+        src_size = int(packet.length)
+        src_count = 1
+    else:
+        # update the variables
+        src_size += int(packet.length)
+        src_count += 1
+    # calculate the mean size
+    smean = src_size / src_count
+    return smean
+
+def get_tcprtt(packet):
+    # get the TCP round trip time (RTT)
+    # assuming that the packets are TCP packets with SYN and ACK flags set
+    global syn_time # keep track of the time when SYN flag is set
+    global ack_time # keep track of the time when ACK flag is set
+    if syn_time is None or ack_time is None:
+        # first packet, initialize the variables
+        syn_time = None
+        ack_time = None
+    # check if SYN flag is set
+    if packet.tcp.flags_syn == '1':
+        # update syn_time with current packet time
+        syn_time = packet.sniff_time
+    # check if ACK flag is set
+    if packet.tcp.flags_ack == '1':
+        # update ack_time with current packet time
+        ack_time = packet.sniff_time
+    
+    if syn_time is not None and ack_time is not None:
+        # calculate RTT in seconds as difference between ack_time and syn_time 
+        delta = ack_time - syn_time 
+        tcprtt = delta.total_seconds()
+        # reset syn_time and ack_time for next RTT calculation 
+        syn_time = None 
+        ack_time = None 
+    else: 
+        # RTT cannot be calculated yet 
+        tcprtt = None 
+    return tcprtt
+
 def capture_packets():
-    # iterate over captured packets
-    for packet in capture.sniff_continuously():
-        # print the captured packet
-        print(packet)
+    # iterate over captured packets 
+    for packet in capture.sniff_continuously(): 
+        # print the captured packet 
+        print(packet) 
+        
+        # extract the features from each packet 
+        # check if the packet is a TCP packet
+        if packet.transport_layer == 'TCP':
+            features['dload'] = float(packet.tcp.window_size_value) * 8 / float(packet.tcp.analysis_ack_rtt) * 1000 / 1024 / 1024 
+            features['ct_state_ttl'] = int(packet.ip.ttl) + int(packet.tcp.flags) 
+            features['rate'] = get_rate(packet) 
+            features['sttl'] = int(packet.ip.ttl) 
+            features['sload'] = float(packet.length) * 8 / float(packet.sniff_timestamp) * 1000 / 1024 / 1024 
+            features['dinpkt'] = float(packet.sniff_timestamp) - float(packet.tcp.time_relative) 
+            features['smean'] = get_smean(packet) 
+            features['tcprtt'] = get_tcprtt(packet) 
+            features['state'] = packet.tcp.state 
+            features['dbytes'] = int(packet.ip.len) - int(packet.ip.hdr_len) - int(packet.tcp.hdr_len) 
+        
+        # print the features for each packet 
+        print(features)
+
+
+# initialize some global variables
+prev_time = None
+src_size = None
+src_count = None
+syn_time = None
+ack_time = None
 
 # create and start a thread to run the packet capture and processing code
 t = threading.Thread(target=capture_packets)
 t.start()
 
-
-
-### save as a script and call that script in my project.ipnb
-###  see in bing chat straight
--- capturezz.py
+++ capturezz.py
@@ -1,108 +1,74 @@
+
 # import pyshark
 # import threading
 
-# # specify the network interface to capture packets from
-# interface = 'wlan0'
+# # Create a packet capture object (you can choose an interface or read from a file)
+# capture = pyshark.LiveCapture(interface='wlan0')  # Replace 'eth0' with your desired interface
 
-# # create a capture object
-# capture = pyshark.LiveCapture(interface=interface)
-
-# def capture_packets():
-#     # iterate over captured packets
-#     for packet in capture.sniff_continuously():
-#         # print the captured packet
-#         print(packet)
+# # Process each packet and extract relevant features
+# for packet in capture.sniff_continuously():
+#     try:
+#         dload = float(packet.ip.len)  # Extract total packet length (downlink)
+#         ct_state_ttl = int(packet.tcp.flags)  # Extract TCP flags (connection state)
         
-#         # Extract features from packet
-#         try:
-#             dload = packet.ip.len  # Assuming 'dload' refers to IP datagram length
-#             ct_state_ttl = packet.ip.ttl  # TTL value from IP header
-#             rate = None  # This might need additional processing based on your specific requirements
-#             sttl = packet.ip.ttl  # TTL value from IP header
-#             sload = None  # This might need additional processing based on your specific requirements
-#             dinpkt = None  # This might need additional processing based on your specific requirements
-#             smean = None  # This might need additional processing based on your specific requirements
-#             tcprtt = packet.tcp.analysis.ack_rtt if hasattr(packet.tcp, 'analysis') else None  # TCP round-trip time
-#             state = packet.tcp.flags.syn == '1' and packet.tcp.flags.ack == '1'  # Check if it's a SYN-ACK packet
-#             dbytes = len(packet.data.data) if hasattr(packet, 'data') else 0  # Data bytes size
-            
-#             print(f'dload: {dload}, ct_state_ttl: {ct_state_ttl}, rate: {rate}, sttl: {sttl}, sload: {sload}, dinpkt: {dinpkt}, smean: {smean}, tcprtt: {tcprtt}, state: {state}, dbytes: {dbytes}')
-            
-#         except AttributeError:
-#             pass  # Not all packets will have all attributes, so we just skip if an attribute is not present
+#         rate = float(packet.ip.ds_field)  # Extract Differentiated Services (DSCP) field
+#         sttl = int(packet.ip.ttl)  # Extract source TTL
+#         sload = float(packet.ip.src)  # Extract source IP address (load)
+#         dinpkt = int(packet.ip.packets)  # Extract number of incoming packets
+#         smean = float(packet.ip.src_port)  # Extract source port (mean)
+#         tcprtt = float(packet.tcp.time_relative)  # Extract TCP relative timestamp
+#         state = packet.tcp.flags_str  # Extract TCP connection state (e.g., SYN, ACK)
+#         dbytes = int(packet.ip.src_oui)  # Extract destination OUI (Organizationally Unique Identifier)
 
-# # create and start a thread to run the packet capture and processing code
+#         # Now use these extracted features for classification
+#         # ...
+#     except AttributeError:
+#         pass  # Skip packets that don't have all required fields
+
+# # Close the capture when done
+# # capture.close()
+
 # t = threading.Thread(target=capture_packets)
 # t.start()
-
-
-
-# import pyshark
-# import threading
-
-# # specify the network interface to capture packets from
-# interface = 'wlan0'
-
-# # create a capture object
-# capture = pyshark.LiveCapture(interface=interface)
-
-# def capture_packets():
-#     # iterate over captured packets
-#     for packet in capture.sniff_continuously():
-#         # Extract features from packet
-#         try:
-#             dload = packet.ip.len  # Assuming 'dload' refers to IP datagram length
-#             ct_state_ttl = packet.ip.ttl  # TTL value from IP header
-#             rate = None  # This might need additional processing based on your specific requirements
-#             sttl = packet.ip.ttl  # TTL value from IP header
-#             sload = None  # This might need additional processing based on your specific requirements
-#             dinpkt = None  # This might need additional processing based on your specific requirements
-#             smean = None  # This might need additional processing based on your specific requirements
-#             tcprtt = packet.tcp.analysis.ack_rtt if hasattr(packet.tcp, 'analysis') else None  # TCP round-trip time
-#             state = packet.tcp.flags.syn == '1' and packet.tcp.flags.ack == '1'  # Check if it's a SYN-ACK packet
-#             dbytes = len(packet.data.data) if hasattr(packet, 'data') else 0  # Data bytes size
-            
-#             print(f'dload: {dload}, ct_state_ttl: {ct_state_ttl}, rate: {rate}, sttl: {sttl}, sload: {sload}, dinpkt: {dinpkt}, smean: {smean}, tcprtt: {tcprtt}, state: {state}, dbytes: {dbytes}')
-            
-#         except AttributeError:
-#             pass  # Not all packets will have all attributes, so we just skip if an attribute is not present
-
-# # create and start a thread to run the packet capture and processing code
-# t = threading.Thread(target=capture_packets)
-# t.start()
-
-
-
-
 
 import pyshark
 import threading
 
-# Create a packet capture object (you can choose an interface or read from a file)
-capture = pyshark.LiveCapture(interface='wlan0')  # Replace 'eth0' with your desired interface
+# specify the network interface to capture packets from
+interface = 'wlan0'
 
-# Process each packet and extract relevant features
-for packet in capture.sniff_continuously():
-    try:
-        dload = float(packet.ip.len)  # Extract total packet length (downlink)
-        ct_state_ttl = int(packet.tcp.flags)  # Extract TCP flags (connection state)
-        
-        rate = float(packet.ip.ds_field)  # Extract Differentiated Services (DSCP) field
-        sttl = int(packet.ip.ttl)  # Extract source TTL
-        sload = float(packet.ip.src)  # Extract source IP address (load)
-        dinpkt = int(packet.ip.packets)  # Extract number of incoming packets
-        smean = float(packet.ip.src_port)  # Extract source port (mean)
-        tcprtt = float(packet.tcp.time_relative)  # Extract TCP relative timestamp
-        state = packet.tcp.flags_str  # Extract TCP connection state (e.g., SYN, ACK)
-        dbytes = int(packet.ip.src_oui)  # Extract destination OUI (Organizationally Unique Identifier)
+# create a capture object
+capture = pyshark.LiveCapture(interface=interface)
 
-        # Now use these extracted features for classification
-        # ...
-    except AttributeError:
-        pass  # Skip packets that don't have all required fields
+syn_time = None  # keep track of the time when SYN flag is set
+ack_time = None  # keep track of the time when ACK flag is set
 
-# Close the capture when done
-# capture.close()
+def get_tcprtt(packet):
+    global syn_time, ack_time
+    if packet.tcp.flags_syn == '1':
+        syn_time = packet.sniff_time
+    if packet.tcp.flags_ack == '1':
+        ack_time = packet.sniff_time
+    if syn_time is not None and ack_time is not None:
+        delta = ack_time - syn_time 
+        tcprtt = delta.total_seconds()
+        syn_time = None 
+        ack_time = None 
+    else: 
+        tcprtt = None 
+    return tcprtt
 
+def capture_packets():
+    # iterate over captured packets
+    for packet in capture.sniff_continuously():
+        # Extract TCP RTT from packet
+        try:
+            tcprtt = get_tcprtt(packet)
+            if tcprtt is not None:
+                print(f'TCP RTT: {tcprtt} seconds')
+        except AttributeError:
+            pass  # Not all packets will have TCP flags, so we just skip if an attribute is not present
+
+# create and start a thread to run the packet capture and processing code
 t = threading.Thread(target=capture_packets)
 t.start()
